59749926ddd95fc13c01e44d35d98f030ab3f8b9
Yusuf Jamal <148943469+jam92444@users.noreply.github.com>
2025-03-08 21:15:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Ensure these methods are allowed
 }));
+app.options('*', cors());
 
 //api endpointes 
 app.get('/',(req,res)=>{
